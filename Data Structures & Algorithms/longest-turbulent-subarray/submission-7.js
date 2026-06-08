d4ca439239e34686743bff48d2f978d58721aa2e
Jae <86385803+DevJaerl@users.noreply.github.com>
2026-06-07 21:05:39 -0400

    Add: longest-turbulent-subarray - submission-7

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-7.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number}
+     */
+    maxTurbulenceSize(arr) {
+        if (arr.length == 1) return 1;
+        if (arr.length == 2 && !(arr[0] == arr[1])) return 2;
+
+        let length = 1;
+        let L = 0;
+        let direction = "";
+
+        for (let R = 1; R < arr.length; R++) {
+            let currentDirection = "";
+            if (arr[R - 1]<arr[R]) {
+               currentDirection = "^"
+            } else if (arr[R - 1]>arr[R]) {
+                currentDirection = "v"
+            } else {
+               currentDirection = "="
+               L = R
+            }
+
+            console.log(direction, currentDirection)
+            if (direction == currentDirection){ L = R-1}
+
+            length = Math.max(R - L+1, length);
+            !(currentDirection=="=")? direction = currentDirection : direction = ""
+        }
+        return length;
+    }
+}
