5280e524bd5f9c7ccf2ff4fb78075c1db6978c18
Jae <86385803+DevJaerl@users.noreply.github.com>
2026-06-11 15:42:59 -0400

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    pivotIndex(nums) {
+        if (nums.length == 1) return 0;
+        let leftSum = new Array(nums.length).fill(0);
+        let rightSum = new Array(nums.length).fill(0);
+
+        leftSum[1] = nums[0];
+        rightSum[nums.length - 2] = nums[nums.length - 1];
+        for (let i = 2; i < nums.length; i++) {
+            leftSum[i]=leftSum[i-1]+nums[i-1]
+        }
+
+        for (let i = nums.length - 3; i > -1; i--) {
+            rightSum[i]= rightSum[i+1]+nums[i+1]
+        }
+        
+        for (let i = 0; i < nums.length; i++) {
+            if (leftSum[i] == rightSum[i]) return i;
+        }
+
+        return -1;
+    }
+}
